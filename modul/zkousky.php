0be6fd89c88f7d30a2f6b52406bd3106285666b4
luukec@gmail.com <luukec@gmail.com@b66c0ce0-c0c3-11dd-bb00-bd6ab96fe29a>
2009-01-04 12:32:31 +0000

    tak hotova aj učitelská část.. chybí pouze přidat a upravit termín zkoušky.. a u studentu vypsat vysledky zkoušek.. to nevim jak na to.. jinak kompletní

--- modul/zkousky.php
+++ modul/zkousky.php
@@ -17,14 +17,14 @@ class zkousky
     //modul který se zobrazí studentovi - ovìøení pøes CAuth - pøístup má admin a student
     public function student()
     {
-      $polePredmetu=predmety();                   //metoda nám vrátí pøedmìty který student studuje
+      $polePredmetu=predmetyStudent();                   //metoda nám vrátí pøedmìty který student studuje
       $m = count($polePredmetu);                  //poèet hodnot pøenesených v poli pøedmìtù
       $i = 0;                                     //nastavení iteraèní prom. pro cyklus while
         //cyklus while, který vypisuje informace k zadaným pøedmìtùm
         while($i<=$m)//pojede tak dlouho dokud i nebude stejnì velké nebo menší jako poèet rozdìlených hodnot
           {
           //naètení hodnot z databáze o jednotlivých pøedmìtech
-          $sqlZkouska = "SELECT uzivatel_zkousejici,ID_zkouska,ID_predmet,ID_ucebna,zacatek,konec,kapacita FROM `zkouska_termin` WHERE `ID_predmet`='$polePredmetu[$i]'";
+          $sqlZkouska = "SELECT ID_uzivatel_zkousejici,ID_zkouska,ID_predmet,ID_ucebna,zacatek,konec,kapacita FROM `zkouska_termin` WHERE `ID_predmet`='$polePredmetu[$i]'";
           $dataZkouska = $pdo->query($sqlZkouska)->fetch();
           //------------------------
 
@@ -44,24 +44,15 @@ class zkousky
           $i++;                          //inkrementace iteraèní prom.
           }
         //konec cyklu------------------------
-
+    return $zobraz;
     }
     //konec metody student
-
-    //modul který se zobrazí ucitelovi  - ovìøení pøes CAuth - pøístup má admin a uèitel
-    public function ucitel()
-    {
-    return 1;
-    }
-    //konec metody ucitel
-
-
 
     //metoda která nám vrátí pøedmìty které daný uživatel studuje
     private function predmetyStudent();
     {
     //naètení hodnot z databáze o jednotlivých pøedmìtech - pøes funkci polozkaVdb
-    $hodiny =polozkaVdb("hodina_student","hodina_student","WHERE `ID_uzivatel_student`='".$this->ID_uzivatel."'");//vkládá se mysql_tabulka,sloupec,omezujici dotaz
+    $hodiny =polozkaVdb("hodina_student","ID_hodina","WHERE `ID_uzivatel_student`='".$this->ID_uzivatel."'");//vkládá se mysql_tabulka,sloupec,omezujici dotaz
     //------------------------
     $i=0;//iteraèní prom pro cyklus s jednotlivými hodinamy
     $j=0;//iteraèní prom pro cyklus s jednotlivými hodinamy - ale ukládání hodnot do cyklu
@@ -73,9 +64,95 @@ class zkousky
         {
         $j++;//v pøípadì že pole neobsahuje hodnotu, navýšíme hodnotu pro zapisování, jinak se bude hodnota pøepisovat $predmety[$j] - abysme mìli pouze jeden výskyt pøedmìtu
         }
+      $++
       }
 
       return $predmety;//vrací ID na pøedmìty které student studuje
+    }
+    //konec metody predmety
+
+    //modul který se zobrazí ucitelovi  - ovìøení pøes CAuth - pøístup má admin a uèitel
+    public function ucitel()
+    {
+      $polePredmetu=predmetyUcitel();                   //metoda nám vrátí pøedmìty který uèitel uèí
+      $m = count($polePredmetu);                  //poèet hodnot pøenesených v poli pøedmìtù
+      $i = 0;                                     //nastavení iteraèní prom. pro cyklus while
+        //cyklus while, který vypisuje informace k zadaným pøedmìtùm
+        while($i<=$m)//pojede tak dlouho dokud i nebude stejnì velké nebo menší jako poèet rozdìlených hodnot
+          {
+          //naètení hodnot z databáze o jednotlivých pøedmìtech
+          $sqlZkouska = "SELECT ID_zkouska,ID_predmet,ID_ucebna,zacatek,konec,kapacita FROM `zkouska_termin` WHERE `ID_predmet`='$polePredmetu[$i]' and ID_uzivatel_zkousejici='".$this->ID_uzivatel."'";
+          $dataZkouska = $pdo->query($sqlZkouska)->fetch();
+          //------------------------
+
+          // pole hodnot pro smárty
+          $zobraz["ucebna"][$i]=polozkaVdb("ucebna","nazev","WHERE ID_ucebna = ".$dataZkouska["ID_ucebna"]); //název uèebny - voláme funkci, která nám vrátí hodnotu sloupce nazev v tabulce uèeben
+          $zobraz["kapacita"][$i]=$dataZkouska["kapacita"];  //kpacita na zkoušku
+          $zobraz["obsazeno"][$i]=count(obsazenostZkousky($dataZkouska["ID_zkouska"]));//poèet již pøihlášených lidí
+          $zobraz["zacatek"][$i]=$dataZkouska["zacatek"]; //zaèátek zápisu
+          $zobraz["konec"][$i]=$dataZkouska["konec"];     //konec zápisu
+          }
+          return $zobraz;
+    }
+    //konec metody ucitel
+
+    //metoda která pøidá zkoušku
+    public function pridatTerminZkousky()
+    {
+    return 1;
+    }
+    //konec funkce pøidat zkoušku
+
+    //metoda která upraví zkoušku
+    public function upravitTerminZkousky()
+    {
+    return 1;
+    }
+    //konec funkce upravit zkoušku
+
+    //metoda která zobrazí studentovi výsledky jeho zkoušek
+    public function vysledkyZkousekStudent()
+    {
+    return 1;
+    }
+    //konec funkce zobrazit výsledky
+
+    //metoda která zobrazení studenty kteøí sou pøihlášeni na zkoušku
+    public function obsazeniZkousky($ID_zkouska);//pøedává se ID_zkoušky u které to chcem zjistit
+    {
+    $studenti = obsazenostZkousky($ID_zkouska); //uloží id studentù získané z funkce obsazenostZkousky do pole
+    $i=0;//iteraèní prom pro cyklus 1 s jednotlivými studenty
+    $m = count($studenti);//spoèítá studenty
+    while($i<$m)//cyklus 1
+      {
+      $zobraz["jmeno"][$i]=polozkaVdb("uzivatel","jmeno","WHERE ID_uzivatel = ".$studenti[$i]);//naète v db studentovo jmeno a ulozi do noveho pole ktere se pak preda smartum
+      $zobraz["primeni"][$i]=polozkaVdb("uzivatel","primeni","WHERE ID_uzivatel = ".$studenti[$i]); //naète v db studentovo primeni a ulozi do noveho pole ktere se pak preda smartum
+      $i++;
+      }
+      return $zobraz;
+    }
+    //konec funkce upravit zkoušku
+
+    //metoda která nám vrátí pøedmìty které daný uèitel uèí
+    private function predmetyUcitel();
+    {
+    //naètení hodnot z databáze o jednotlivých pøedmìtech - pøes funkci polozkaVdb - vrátí ID_predmet v poli
+    $hodiny =polozkaVdb("hodina","ID_predmet","WHERE `ID_uzivatel_vyucujici`='".$this->ID_uzivatel."'");//vkládá se mysql_tabulka,sloupec,omezujici dotaz
+    //------------------------
+    $i=0;//iteraèní prom pro cyklus s jednotlivými hodinamy
+    $j=0;//iteraèní prom pro cyklus s jednotlivými hodinamy - ale ukládání hodnot do cyklu
+    $m = count($hodiny);
+    while($i<$m)//prvni cyklus
+      {
+      $predmety[$j]=$hodiny[$i];
+      if(zjisteniDuplikace($predmety,$hodiny[$i])==0)//metoda zjisti zda je v poli hodnota $hodiny[$i]
+        {
+        $j++;//v pøípadì že pole neobsahuje hodnotu, navýšíme hodnotu pro zapisování, jinak se bude hodnota pøepisovat $predmety[$j] - abysme mìli pouze jeden výskyt pøedmìtu
+        }
+      $i++;
+      }
+
+      return $predmety;//vrací ID na pøedmìty které uèitel uèí
     }
     //konec metody predmety
 
