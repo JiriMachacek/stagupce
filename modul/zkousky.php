609a3b03eb838ad95958f7f430e929a7597670f7
luukec <luukec@b66c0ce0-c0c3-11dd-bb00-bd6ab96fe29a>
2009-01-18 17:25:35 +0000

    načnuty zkoušky, chybí známkování a přidávání.. taky chce opravit výpis

--- modul/zkousky.php
+++ modul/zkousky.php
@@ -13,193 +13,192 @@ class zkousky
       //------------------------
    }
 //konec metody __konstruct
-    
-    //modul který se zobrazí studentovi - ovìøení pøes CAuth - pøístup má admin a student
-    public function student()
-    {
-      $polePredmetu=predmetyStudent();                   //metoda nám vrátí pøedmìty který student studuje
-      $m = count($polePredmetu);                  //poèet hodnot pøenesených v poli pøedmìtù
-      $i = 0;                                     //nastavení iteraèní prom. pro cyklus while
-        //cyklus while, který vypisuje informace k zadaným pøedmìtùm
-        while($i<=$m)//pojede tak dlouho dokud i nebude stejnì velké nebo menší jako poèet rozdìlených hodnot
-          {
-          //naètení hodnot z databáze o jednotlivých pøedmìtech
-          $sqlZkouska = "SELECT ID_uzivatel_zkousejici,ID_zkouska,ID_predmet,ID_ucebna,zacatek,konec,kapacita FROM `zkouska_termin` WHERE `ID_predmet`='$polePredmetu[$i]'";
-          $dataZkouska = $pdo->query($sqlZkouska)->fetch();
-          //------------------------
+	public function uprav($sl)
+	{
+	//známkování zkoušky
+		return 1;
+	}
+	
+	public function vloz($sl)
+	{
+		$session	= $sl->getSession();
+		$post		= $sl->getPost();
+		$db			= $sl->getDb();
+	
+		$zobraz['typchyba'] = false;
+		$zobraz['typchybaexistuje'] = false;
+		$zobraz['typ'] = '';
+		
+		if(empty($post))
+		{
+			$formular = true;
+		}
+		else
+		{
+			$formular = false;
+			
+			$zobraz['typ'] = $post['typ'];
+			
+			if ($post['typ'] == '') //pokud je typ prázdný zobrazí chybu
+			{
+				$zobraz['nazevchyba'] = true;
+				$formular = true;
+			}
+			else
+			{
+				/**
+				 * testování zda typ uèebny již nenexistuje...
+				 */
+				
+				$sql = "SELECT typ FROM ucebna_typ WHERE typ = '$zobraz[typ]'";
+				
+				$result = $db->query($sql)->fetch();
+				if($result)
+				{
+					$formular = true;
+					$zobraz['typchybaexistuje'] = true;
+				}
+				else
+				{
+					$input['typ'] = $post['typ'];
+				}
+					
+			}
+			
+		}
+		
+		if ($formular)
+		{
+			$sl->zobraz($zobraz, 'zkousky-formular.tpl');
+		}
+		else
+		{
+			try
+			{
+				$db->begintransaction(); //zaèátek transakce
+				
+				$sql = $sl->ArrayToSql($input, 'ucebna_typ');
 
-          // pole hodnot pro smárty
-          $zobraz["zkousejiciJmeno"][$i]=polozkaVdb("uzivatel","jmeno","WHERE ID_uzivatel = ".$dataZkouska["uzivatel_zkousejici"]);  //vyhledá v tabulce uživatelù podle ID jméno
-          $zobraz["zkousejiciPrimeni"][$i]=polozkaVdb("uzivatel","primeni","WHERE ID_uzivatel = ".$dataZkouska["uzivatel_zkousejici"]); //vyhledá v tabulce uživatelù podle ID pøímìní
-          $zobraz["ucebna"][$i]=polozkaVdb("ucebna","nazev","WHERE ID_ucebna = ".$dataZkouska["ID_ucebna"]); //název uèebny - voláme funkci, která nám vrátí hodnotu sloupce nazev v tabulce uèeben
-          $zobraz["kapacita"][$i]=$dataZkouska["kapacita"];  //kpacita na zkoušku
-          $zobraz["obsazeno"][$i]=count(obsazenostZkousky($dataZkouska["ID_zkouska"]));//poèet již pøihlášených lidí
-          $zobraz["zacatek"][$i]=$dataZkouska["zacatek"]; //zaèátek zápisu
-          $zobraz["konec"][$i]=$dataZkouska["konec"];     //konec zápisu
-          $zobraz["zapsan"][$i]=(zjisteniDuplikace(obsazenostZkousky($dataZkouska["ID_zkouska"]),$this->ID_uzivatel));     /*do hodnoty uloží 1 - pokud je uživatel již zapsán, 0 pokud není.
-          Volá to funkci zjisteniDuplikace, která zjistí zda v poli, kterou pøedá zpátky funkce obsazenost zkoušky je ID pøihlášeného uživatele
-          -toto bude použivot na kontrolu zda se uživatel mùže pøihlásit nebo ne*/
+				$db->query($sql);
 
-          //------------------------
-          $i++;                          //inkrementace iteraèní prom.
-          }
-        //konec cyklu------------------------
-    return $zobraz;
-    }
-    //konec metody student
+				$db->commit(); //commitnutí tranaskce
+			}
+			catch (PDOException $e)
+			{
+				/*
+				 * když byla zachycena vyjímka v SQL zobrazí se chyba a konec
+				 */
+				$pdo->rollBack();
+				die($e);
+			}
+			
+			
+			
+			header('location: ./?modul=ucebnytypy&metoda=zobraz');
+		}
+		
+		//var_dump($session);
+	}
+	
+	public function zobraz($sl)
+	{
+		$session	= $sl->getSession(); // zde je vytáhne obsah promìnné $_SESSION
+		$post		= $sl->getPost(); // zde je vytáhne obsah promìnné $_POST
+		$db			= $sl->getDb(); // // zde je vytáhne databázový objekt PDO
+		
+		if (!empty($post))
+		{
+			try
+			{
+				$db->begintransaction(); //zaèátek transakce
+				
+				$db->query("DELETE FROM hodina_student WHERE ID_uzivatel_student = '$session[ID_uzivatel]'");
+				
+				if (isset($post['predmet']))
+				{
+					foreach ($post['predmet'] as $predmet)
+					{
+						$input['ID_uzivatel_student'] = $session['ID_uzivatel'];
+						$input['ID_hodina'] = $predmet;
+						
+						$sql = $sl->ArrayToSql($input, 'hodina_student');
 
-    //metoda která nám vrátí pøedmìty které daný uživatel studuje
-    private function predmetyStudent();
-    {
-    //naètení hodnot z databáze o jednotlivých pøedmìtech - pøes funkci polozkaVdb
-    $hodiny =polozkaVdb("hodina_student","ID_hodina","WHERE `ID_uzivatel_student`='".$this->ID_uzivatel."'");//vkládá se mysql_tabulka,sloupec,omezujici dotaz
-    //------------------------
-    $i=0;//iteraèní prom pro cyklus s jednotlivými hodinamy
-    $j=0;//iteraèní prom pro cyklus s jednotlivými hodinamy - ale ukládání hodnot do cyklu
-    $m = count($hodiny);
-    while($i<$m)//prvni cyklus
-      {
-      $predmety[$j]=$hodiny[$i];
-      if(zjisteniDuplikace($predmety,$hodiny[$i])==0)//metoda zjisti zda je v poli hodnota $hodiny[$i]
-        {
-        $j++;//v pøípadì že pole neobsahuje hodnotu, navýšíme hodnotu pro zapisování, jinak se bude hodnota pøepisovat $predmety[$j] - abysme mìli pouze jeden výskyt pøedmìtu
-        }
-      $++
-      }
+						$db->query($sql);
+					}
+				}
 
-      return $predmety;//vrací ID na pøedmìty které student studuje
-    }
-    //konec metody predmety
+				$db->commit(); //commitnutí tranaskce
+				
+				header('location: ./?modul=zk&metoda=zobraz');
+			}
+			catch (PDOException $e)
+			{
+				/*
+				 * když byla zachycena vyjímka v SQL zobrazí se chyba a konec
+				 */
+				$pdo->rollBack();
+				die($e);
+			}
+		}
+		
+		$sql = "SELECT	zt.ID_zkouska,
+						concat(h.jmeno, ' ',h.primeni) AS zkousejici,
+						u.nazev AS ucebna_nazev,
+						p.nazev AS predmet_nazev,
+						concat(zt.zacatek, ' - ', zt.konec) AS cas,
+						zt.kapacita - count(zs.ID_uzivatel_zkouseny) AS kapacita
+						
+				FROM	zkouska_termin zt
+				LEFT JOIN uzivatel u ON zt.ID_uzivatel_zkousejici = h.ID_uzivatel
+				LEFT JOIN predmet p ON zt.ID_predmet = p.ID_predmet
+				LEFT JOIN zkouska_student p ON zt.ID_zkouska = zs.ID_zkouska
+				LEFT JOIN ucebna p ON zt.ID_ucebna = u.ID_ucebna
+				GROUP BY ID_zkouska
+				
+				";
+		
+	//	$sql = "SELECT ID_hodina FROM ID_hodina_student WHERE ID_uzivatel_student = '$session[ID_uzivatel]'";
+		
+		$zobraz['rozvrh'] = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC); // do promìnné result vytáhne všechny øádky DB ve formátu: 
+		
+		
+		
+		
+		$sl->zobraz($zobraz, 'zkouskaprihlaseni.tpl'); // preda sablone hodnoty pole zobraz a zobrazi je v sablone ucebny.tpl
+	}
+	
+	public function vymaz($sl)
+	{
+		$db			= $sl->getDb(); // // zde je vytáhne databázový objekt PDO
+		$get		= $sl->getGet();
+		
+		if (isset($get['zkouska']))
+		{
+			$id = $get['zkouska'];
+		
+			try
+			{
+				$db->begintransaction(); //zaèátek transakce
 
-    //modul který se zobrazí ucitelovi  - ovìøení pøes CAuth - pøístup má admin a uèitel
-    public function ucitel()
-    {
-      $polePredmetu=predmetyUcitel();                   //metoda nám vrátí pøedmìty který uèitel uèí
-      $m = count($polePredmetu);                  //poèet hodnot pøenesených v poli pøedmìtù
-      $i = 0;                                     //nastavení iteraèní prom. pro cyklus while
-        //cyklus while, který vypisuje informace k zadaným pøedmìtùm
-        while($i<=$m)//pojede tak dlouho dokud i nebude stejnì velké nebo menší jako poèet rozdìlených hodnot
-          {
-          //naètení hodnot z databáze o jednotlivých pøedmìtech
-          $sqlZkouska = "SELECT ID_zkouska,ID_predmet,ID_ucebna,zacatek,konec,kapacita FROM `zkouska_termin` WHERE `ID_predmet`='$polePredmetu[$i]' and ID_uzivatel_zkousejici='".$this->ID_uzivatel."'";
-          $dataZkouska = $pdo->query($sqlZkouska)->fetch();
-          //------------------------
+				$sql = "DELETE FROM zkouska WHERE ID_zkouska = '$id'";
 
-          // pole hodnot pro smárty
-          $zobraz["ucebna"][$i]=polozkaVdb("ucebna","nazev","WHERE ID_ucebna = ".$dataZkouska["ID_ucebna"]); //název uèebny - voláme funkci, která nám vrátí hodnotu sloupce nazev v tabulce uèeben
-          $zobraz["kapacita"][$i]=$dataZkouska["kapacita"];  //kpacita na zkoušku
-          $zobraz["obsazeno"][$i]=count(obsazenostZkousky($dataZkouska["ID_zkouska"]));//poèet již pøihlášených lidí
-          $zobraz["zacatek"][$i]=$dataZkouska["zacatek"]; //zaèátek zápisu
-          $zobraz["konec"][$i]=$dataZkouska["konec"];     //konec zápisu
-          }
-          return $zobraz;
-    }
-    //konec metody ucitel
+				$db->query($sql);
 
-    //metoda která pøidá zkoušku
-    public function pridatTerminZkousky()
-    {
-    return 1;
-    }
-    //konec funkce pøidat zkoušku
-
-    //metoda která upraví zkoušku
-    public function upravitTerminZkousky()
-    {
-    return 1;
-    }
-    //konec funkce upravit zkoušku
-
-    //metoda která zobrazí studentovi výsledky jeho zkoušek
-    public function vysledkyZkousekStudent()
-    {
-    return 1;
-    }
-    //konec funkce zobrazit výsledky
-
-    //metoda která zobrazení studenty kteøí sou pøihlášeni na zkoušku
-    public function obsazeniZkousky($ID_zkouska);//pøedává se ID_zkoušky u které to chcem zjistit
-    {
-    $studenti = obsazenostZkousky($ID_zkouska); //uloží id studentù získané z funkce obsazenostZkousky do pole
-    $i=0;//iteraèní prom pro cyklus 1 s jednotlivými studenty
-    $m = count($studenti);//spoèítá studenty
-    while($i<$m)//cyklus 1
-      {
-      $zobraz["jmeno"][$i]=polozkaVdb("uzivatel","jmeno","WHERE ID_uzivatel = ".$studenti[$i]);//naète v db studentovo jmeno a ulozi do noveho pole ktere se pak preda smartum
-      $zobraz["primeni"][$i]=polozkaVdb("uzivatel","primeni","WHERE ID_uzivatel = ".$studenti[$i]); //naète v db studentovo primeni a ulozi do noveho pole ktere se pak preda smartum
-      $i++;
-      }
-      return $zobraz;
-    }
-    //konec funkce upravit zkoušku
-
-    //metoda která nám vrátí pøedmìty které daný uèitel uèí
-    private function predmetyUcitel();
-    {
-    //naètení hodnot z databáze o jednotlivých pøedmìtech - pøes funkci polozkaVdb - vrátí ID_predmet v poli
-    $hodiny =polozkaVdb("hodina","ID_predmet","WHERE `ID_uzivatel_vyucujici`='".$this->ID_uzivatel."'");//vkládá se mysql_tabulka,sloupec,omezujici dotaz
-    //------------------------
-    $i=0;//iteraèní prom pro cyklus s jednotlivými hodinamy
-    $j=0;//iteraèní prom pro cyklus s jednotlivými hodinamy - ale ukládání hodnot do cyklu
-    $m = count($hodiny);
-    while($i<$m)//prvni cyklus
-      {
-      $predmety[$j]=$hodiny[$i];
-      if(zjisteniDuplikace($predmety,$hodiny[$i])==0)//metoda zjisti zda je v poli hodnota $hodiny[$i]
-        {
-        $j++;//v pøípadì že pole neobsahuje hodnotu, navýšíme hodnotu pro zapisování, jinak se bude hodnota pøepisovat $predmety[$j] - abysme mìli pouze jeden výskyt pøedmìtu
-        }
-      $i++;
-      }
-
-      return $predmety;//vrací ID na pøedmìty které uèitel uèí
-    }
-    //konec metody predmety
-
-    //metoda která nám zjišuje zda v daném poli je pøenášená hodnota již obsazena -pokud je, vrátí 0, pokud není, vrátí 1
-    private function zjisteniDuplikace($pole,$hodnota);
-    {
-    $i=0;//iteraèní prom pro cyklus s jednotlivými záznamy
-    $m = count($pole); //zjištìní poètu záznamù v poli
-    $jeTam=0; //nastavení návratové hodnoty na 0 - tímto se vyhneme psali else vìtví v podmínce 1, pokud se záznamy rovnat nebudou, hodnota se nezmìní
-      while($j<$m) //cyklus který projíždí záznamy v poli
-          {
-          if($pole[$i]==$hodnota) // podmínka 1 která porovnává zda v poli je pøedaná hodnota
-            {
-            $jeTam = 1;//v pøípadì že se hodnota se záznamem v poli rovná, vrátíme hodnotu
-            }
-          }
-    return $jeTam; //vrácení hodnoty zda v pøedáném poli pedaná hodnota je
-    }
-    //konec metody zkouskyPokusyStudent
-
-    //metoda která nám vypíše pokusy zkoušek - tedy výsledky
-    private function zapsatNaZkousku($ID_zkouska);
-    {
-    return 1; //nevím jak se objektovì ukládá do DB.. ale staèí tady do databáze uložit $this->ID_uzivatel, id zkoušky se pøedává v hlavièce
-    }
-    //konec metody zkouskyPokusyStudent
-
-    //metoda která nám vypíše pokusy zkoušek
-    private function obsazenostZkousky($ID_zkouska);
-    {
-    //naètení studentù z databáze k urèení obsazenosti dané zkoušky
-    $sqlZkouska = "SELECT uzivatel_zkousejici FROM `zkouska_student` WHERE `ID_zkouska`='$ID_zkouska'";
-    $dataZkouska = $pdo->query($sqlZkouska)->fetch();
-    //------------------------
-
-    return  $dataZkouska["ID_uzivatel_zkouseny"]; // vrátí pole s uživateli, kteøí sou pøihlášeni na zoušku - tedy jejich id
-    }
-    //konec metody zkouskyPokusyStudent
-
-    //metoda která nám vypíše položku v db
-    private function polozkaVdb($tabulka,$sloupec,$omezeni);
-    {
-    //naètení hodnoty z databáze podle pøedaných parametrù
-    $sqlPolozka = "SELECT $soupev FROM $tabulka $omezeni";
-    $dataPolozka = $pdo->query($sqlPolozka)->fetch();
-    //------------------------
-    return $dataPolozka[$sloupec]; //vrácení nalezené hodnoty;
-    }
-    //konec metody zkouskyPokusyStudent
+				$db->commit(); //commitnutí tranaskce
+			}
+			catch (PDOException $e)
+			{
+				/**
+				 * když byla zachycena vyjímka v SQL zobrazí se chyba a konec
+				 */
+				$pdo->rollBack();
+				die($e);
+			}
+		
+		}
+		
+		header('location: ./?modul=zkousky&metoda=zobraz');
+	}
+	
 }
+?>
